8624af20aeaef6d148cb20dd67d05a9e4b04ca2d
Devraj Singh <dsdevrajsingh55555@gmail.com>
2023-11-25 22:59:05 +0530

    desktop version done

--- src/components/banner/BannerComponent.jsx
+++ src/components/banner/BannerComponent.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
 import { Carousel } from "react-bootstrap";
+import "./BannerComponent.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { items } from "../../../public/CarouselItems.json";
 
@@ -14,13 +15,25 @@ export default function BannerComponent() {
     setIndex(selectedIndex);
   };
 
+  const splitTextIntoWords = (text) => {
+    return text.split(' ').map((word, index) => (
+      <div key={index} className="word">
+        {word}
+      </div>
+    ));
+  };
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} indicators={false}>
       {carouselItems.map((item) => (
         <Carousel.Item key={item.id}>
           <img src={item.imageUrl} alt="slides" />
           <Carousel.Caption>
-            <h3>{item.heading}</h3>
+            <div style={{ textAlign: `${item.position}`, top: `${item.top}`, left: `${item.left}` }} className="carousel-heading">
+              {/* {item.heading} */}
+              {splitTextIntoWords(item.heading)}
+            </div>
+            <div className="shop-button">shop now</div>
           </Carousel.Caption>
         </Carousel.Item>
       ))}
